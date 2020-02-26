caf609edf900cb8be528a5195c89a6b2092fedd0
luisfff29 <luisfff29@gmail.com>
2020-02-26 11:39:19 -0600

    print paths + filename

--- copyspecial.py
+++ copyspecial.py
@@ -20,16 +20,29 @@ import argparse
 __author__ = "luisfff29"
 
 
-# +++your code here+++
 # Write functions and modify main() to call them
+def get_special_paths(dirname):
+    """Fin all special files and list them"""
+    for p, d, f in os.walk(dirname, topdown=True):
+        if "/." not in p:
+            for files in f:
+                print(os.path.join(p, files))
+
 
 def main():
     # This snippet will help you get started with the argparse module.
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Take a directory as an argument")
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
+    parser.add_argument('dir', help='dir path')
     # TODO need an argument to pick up 'from_dir'
     args = parser.parse_args()
+
+    if args.dir == ".":
+        my_dir = os.getcwd()
+
+    get_special_paths(my_dir)
 
     # TODO you must write your own code to get the cmdline args.
     # Read the docs and examples for the argparse module about how to do this.
